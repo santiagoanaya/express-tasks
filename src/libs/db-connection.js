692a3a950901f11c2162a4668f4f4bd948382954
santiagoanaya <anayasantiagohernan@gmail.com>
2018-10-19 11:57:03 -0300

    Task list

---
+++ src/libs/db-connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+let db
+
+module.exports = function Connection() {
+  if(!db) {
+    db = mongoose.connect('mongodb://localhost/crud-example', {
+      useMongoClient: true
+    })
+  }
+
+  return db
+}
